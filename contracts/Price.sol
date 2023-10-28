e99de1072fd5abfdda9900f301a81505e8ccc7c8
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-10-28 12:35:52 +0530

    price

---
+++ contracts/Price.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriveConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
+
+    function getLatestPrice() public view returns(int) {
+        (,int price,,,) = priceFeed.latestRoundData();
+    } 
+
+}
